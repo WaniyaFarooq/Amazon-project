3c30f473619929e6b8baf2987d76c89021369ec4
Waniya <waniyafarooq18@gmail.com>
2025-08-31 05:08:33 +0500

    Make the cart Quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -76,6 +76,10 @@ cart.push(
     quantity: 1
   });
 }
-
+let cartQuantity = 0;
+cart.forEach((item)=>{
+cartQuantity+=item.quantity;
+});
+document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
   });
 });
