5ee39c22947fc5c0ed142c1fc8c81489b7b1539e
Waniya <waniyafarooq18@gmail.com>
2025-08-31 23:34:48 +0500

    generated HTML for order summary

--- data/cart.js
+++ data/cart.js
@@ -1,6 +1,12 @@
 export const cart = [
-
-]
+    {
+ productId:"83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+ quantity:2
+},
+{ productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+ quantity:7
+}
+];
 
 export function addToCart(productId){
   let match;
