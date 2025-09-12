747530f9604bf4bdaccb98acadfc1643eb5bae1e
Waniya <waniyafarooq18@gmail.com>
2025-09-12 10:50:44 +0500

    Add backend-practice.js f

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,7 +2,9 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 
 import {renderCheckoutHeader} from './checkout/checkoutHeader.js';
  import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+ import '../data/backend-practice.js';
 renderOrderSummary();
 renderPaymentSummary();
 renderCheckoutHeader();
-// import  "../data/cart-class.js";
+// // import  "../data/cart-class.js";
+// 
